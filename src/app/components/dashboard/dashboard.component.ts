8a34d4264ca723523eec2e49423d0917ef0c48f9
Geoffrey Sandja <geoffreysandja@gmail.com>
2019-04-03 12:39:44 +0530

    Adding Dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StocksService,StockInterface} from '../../services/stocks.service';
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  stocks: Array<StockInterface>;
+  symbols: Array<string>;
+  constructor(private service: StocksService) {
+    this.symbols=service.get();
+   }
 
   ngOnInit() {
+    this.service.load(this.symbols).subscribe(stocks=> this.stocks=stocks);
   }
+  
 
 }
